75d31b31ec4042994f9e7dbe36301831ce511d5d
Joyce <joyceismyname.liuyy@gmail.com>
2023-06-20 11:47:56 +1200

    error trapping complete

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 from tkinter import * 
+from tkinter import messagebox
 import PIL
 from PIL import ImageTk
 from PIL import Image
@@ -6,7 +7,10 @@ root = Tk()
 
 root.geometry()
 root.title("Eggs Tuckshop ordering system")
+debug1 = StringVar
 
+# list to store food items the user has ordered
+ordered_food = []
 
 # welcome message
 welcome_message = Label(text = "Welcome to the EGGS tuckshop ordering page" , font= ('Arial', 20), )
@@ -197,12 +201,13 @@ new_cupcake = ImageTk.PhotoImage(cupcake_img)
 cupcake_label = Label(root, image=new_cupcake)
 cupcake_label.grid(row = 12, column=3)
 
-# entry
-chipEntry = Entry(root)
-chipEntry.grid(row = 4, column=0)
 
-burgerEntry = Entry(root)
-burgerEntry.grid(row = 7, column=0)
+# entry
+chip_entry = Entry(root)
+chip_entry.grid(row = 4, column=0)
+
+burger_entry = Entry(root)
+burger_entry.grid(row = 7, column=0)
 
 pastaEntry = Entry(root)
 pastaEntry.grid(row = 10, column=0)
@@ -248,13 +253,195 @@ cupcakeEntry.grid(row = 13, column=3)
 # entry end
 
 
-# open checkout 
-def open_checkout():
-    checkout_window = Toplevel(root)
 
-    checkout_window.title("Checkout")
-    checkout_welcome = Label(checkout_window, text="Check Out", font= ('Arial', 20))
-    checkout_welcome.grid( row=0, column= 1, columnspan=2, padx = 50, pady = 20,)
+
+# a function that calculate the checkouts
+def checkout():
+    
+    chip_value = chip_entry.get()
+    burger_value = burger_entry.get()
+    pasta_value = pastaEntry.get()
+    soup_value = soupEntry.get()
+    fruits_value = fruitsEntry.get()
+    sushi_value = sushiEntry.get()
+    sandwich_value = sandwichEntry.get()
+    eggs_value = eggsEntry.get()
+    cola_value = colaEntry.get()
+    milkshake_value = milkshakeEntry.get()
+    orangej_value = orangejEntry.get()
+    applej_value = applejEntry.get()
+    brownie_value = brownieEntry.get()
+    cookie_value = cookieEntry.get()
+    icec_value = icecEntry.get()
+    cupcake_value = cupcakeEntry.get()
+
+# checking if there is quantity inputed if not the value will be set to 0
+    if len(chip_value) == 0:
+        chip_value = 0
+    if len(burger_value) == 0:
+        burger_value = 0
+    if len(soup_value) == 0:
+        soup_value = 0
+    if len(pasta_value) == 0:
+        pasta_value = 0
+    if len(fruits_value) == 0:
+       fruits_value = 0
+    if len(sushi_value) == 0:
+        sushi_value = 0
+    if len(sandwich_value) == 0:
+        sandwich_value = 0
+    if len(eggs_value) == 0:
+        eggs_value = 0
+    if len(cola_value) == 0:
+        cola_value = 0
+    if len(milkshake_value) == 0:
+        milkshake_value = 0
+    if len(orangej_value) == 0:
+        orangej_value = 0
+    if len(applej_value) == 0:
+        applej_value = 0
+    if len(brownie_value) == 0:
+        brownie_value = 0
+    if len(icec_value) == 0:
+        icec_value = 0
+    if len(cookie_value) == 0:
+        cookie_value = 0
+    if len(cupcake_value) == 0:
+        cupcake_value = 0
+
+    # error trapping for letters
+    try:
+        chip_value = int(chip_value)    
+        burger_value = int(burger_value)
+        pasta_value = int(pasta_value)
+        soup_value = int(soup_value)
+        fruits_value = int(fruits_value)
+        sushi_value = int(sushi_value)
+        sandwich_value = int(sandwich_value)
+        eggs_value = int(eggs_value)
+        cola_value = int(cola_value)
+        milkshake_value = int(milkshake_value)
+        orangej_value = int(orangej_value)
+        applej_value = int(applej_value)
+        brownie_value = int(brownie_value)
+        cookie_value = int(cookie_value)
+        icec_value = int(icec_value)
+        cupcake_value = int(cupcake_value)
+        
+        global occurTrouble
+        occurTrouble = False
+    # error trapping for numbers not between 1-100
+        if chip_value >= 100 or chip_value < 0:
+
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif burger_value >= 100 or burger_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif pasta_value >= 100 or pasta_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif soup_value >= 100 or soup_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")        
+        elif fruits_value >= 100 or fruits_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif sushi_value >= 100 or sushi_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif sandwich_value >= 100 or sandwich_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif eggs_value >= 100 or eggs_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif cola_value >= 100 or cola_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif orangej_value >= 100 or orangej_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif applej_value >= 100 or applej_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif brownie_value >= 100 or brownie_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif cookie_value >= 100 or cookie_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif icec_value >= 100 or icec_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif cupcake_value >= 100 or cupcake_value < 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+        elif chip_value == 0 and burger_value == 0 and pasta_value == 0 and soup_value == 0 \
+            and fruits_value == 0 and sushi_value == 0 and sandwich_value == 0 and eggs_value == 0\
+            and cola_value == 0 and milkshake_value == 0 and orangej_value == 0 and applej_value == 0\
+            and brownie_value == 0 and cookie_value == 0 and icec_value == 0 and cupcake_value == 0:
+            occurTrouble = True
+            messagebox.showerror("error", "Please enter a number between 1 - 100")
+
+    except:
+        occurTrouble = True
+        messagebox.showerror("error", "Please only enter a positive whole number")
+    
+
+
+
+
+
+
+def open_checkout():
+        if not occurTrouble: #checking whether a error has occured for the inputed quantity
+            checkout_window = Toplevel(root) #if not the checkout window will open
+
+            checkout_window.title("Checkout")
+            checkout_welcome = Label(checkout_window, text="Check Out", font= ('Arial', 20))
+            checkout_welcome.grid( row=0, column= 1, columnspan=2, padx = 50, pady = 20,)
+
+            # # price of food
+            # chip_price = chip_value * 4
+            # burger_price = burger_value * 5
+            # pasta_price = pasta_value * 5
+            # soup_price = soup_value * 4.5
+            # fruits_price = fruits_value * 3
+            # sushi_price = sushi_value * 5.5
+            # sandwich_price = sandwich_value * 5
+            # eggs_prcie = eggs_value * 4.5
+            # cola_price = cola_value * 2
+            # milkshake_price = milkshake_value * 2.5
+            # orangej_price = orangej_value * 2.5
+            # applej_price = applej_value * 2.5
+            # brownie_price = brownie_value * 3
+            # cookies_price = cookie_value * 2.5
+            # icec_price = icec_value* 2.5
+            # cupecake_price = cupcake_value * 3
+            
+
+
+            
+
+
+
+
+# buttons
+check_out = Button(root, text = 'Check Out', height = 5, width = 10, command = lambda:[checkout(), open_checkout()] )
+check_out.grid(row = 9, column=4, rowspan= 2, padx= 10)
+
+
+
+            
+
+
+
+
+
+# open checkout 
+
+
 
 # open help
 def open_help():
@@ -266,24 +453,22 @@ def open_help():
     help_text = Label(help_window, text="Welcome to the EGGS tuck shop ordering system\n"
                                         "If you wish you order, you can do so by entering\n"
                                         "the quantity you want under your desired food item\n "
+                                        "(Please only enter a positive wholenumber under 100)\n"
                                         "After you have finished ordering, press on the checkout\n"
                                         "button and a summary of your order, you will be required\n"
                                         "to enter the name for the order along with the option\n"
                                         "to deliver or pick up your order", font= ('Arial', 14))
     help_text.grid(row = 2, column=2, columnspan=3, padx = 50, pady = 50)
 
+help = Button(root, text = 'Help', height = 5, width = 10,command = open_help)
+help.grid(row = 11, column=4, rowspan= 2, padx= 10)
+ 
+reset = Button(root, text = 'Reset', height = 5, width = 10, )
+reset.grid(row = 13, column=4, rowspan= 2, pady= 10)
 
 # reset
 
 
-# buttons
-check_out = Button(root, text = 'Check Out', height = 5, width = 10, command = open_checkout)
-check_out.grid(row = 9, column=4, rowspan= 2, padx= 10)
 
-help = Button(root, text = 'Help', height = 5, width = 10,command = open_help)
-help.grid(row = 11, column=4, rowspan= 2, padx= 10)
-
-reset = Button(root, text = 'Reset', height = 5, width = 10, )
-reset.grid(row = 13, column=4, rowspan= 2, pady= 10)
 
 root.mainloop()
